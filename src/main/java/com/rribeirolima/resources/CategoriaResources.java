ca26a0c08a9e4706b46398afa157921248886a9e
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-01-19 01:56:06 -0300

    Testando a classe Categoria

--- src/main/java/com/rribeirolima/resources/CategoriaResources.java
+++ src/main/java/com/rribeirolima/resources/CategoriaResources.java
@@ -1,16 +1,30 @@
 package com.rribeirolima.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rribeirolima.domain.Categorias;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionado!";
+	public List<Categorias> listar() {
+		
+		Categorias categoria1 = new Categorias(1, "Informática");
+		Categorias categoria2 = new Categorias(2, "Escritório");
+		
+		List<Categorias> lista = new ArrayList<>();
+		
+		lista.add(categoria1);
+		lista.add(categoria2);
+		
+		return lista;
 	}
 	
 }
