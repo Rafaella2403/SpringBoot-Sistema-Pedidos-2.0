517bb24629b2755b13cb23eb247d3d902563e4a3
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-01-19 01:32:44 -0300

    Projeto Criado

---
+++ src/main/java/com/rribeirolima/ImplementandoModeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.rribeirolima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementandoModeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementandoModeloConceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rribeirolima/ImplementandoModeloConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rribeirolima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementandoModeloConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
