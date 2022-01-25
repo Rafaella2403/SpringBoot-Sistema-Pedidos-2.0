0c241f4d5fa782c663b9d04985c1703d2b36d420
Rafaella2403 <rribeirolimabritto@gmail.com>
2022-01-25 00:40:06 -0300

    Criando operação de instanciação

--- src/main/java/com/rribeirolima/ImplementandoModeloConceitualApplication.java
+++ src/main/java/com/rribeirolima/ImplementandoModeloConceitualApplication.java
@@ -1,13 +1,36 @@
 package com.rribeirolima;
 
+import java.util.Arrays;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ImplementandoModeloConceitualApplication {
+import com.rribeirolima.domain.Categoria;
+import com.rribeirolima.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ImplementandoModeloConceitualApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ImplementandoModeloConceitualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//O id esta nulo por que é o banco de dados que vai gerar o id
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
